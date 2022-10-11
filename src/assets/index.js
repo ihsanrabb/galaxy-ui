8542e6c1648ac0b45be63db6e9deb51426dc15bb
Ihsanuddin rabbani <ihsanuddin.rabbani@stockbit.com>
2022-10-11 15:12:46 +0700

    styled component

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import SignUp from './illustration/SignUp.svg';
+
+export * from './icons/CloseIcon';
+
+export const Illustrations = {
+  SignUp
+};
--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import PrimaryButton, {
+  SecondaryButton,
+  TertiaryButton
+} from './components/Button';
+import { GlobalStyle, darkTheme, defaultTheme } from './utils';
+import { SignUpModal } from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  const [useDarkTheme, setUseDarkTheme] = React.useState(false);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    <ThemeProvider theme={useDarkTheme ? darkTheme : defaultTheme}>
+      <button onClick={() => setUseDarkTheme(prevState => !prevState)}>change theme</button>
+      <h1>Hello world!</h1>
+      <div
+        style={{
+          background: useDarkTheme
+            ? defaultTheme.primaryColor
+            : darkTheme.primaryColor,
+          width: "100vw",
+          height: "90vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-around"
+        }}
+      >
+        <PrimaryButton modifiers={["small", "primaryButtonWarning"]}>asdsd</PrimaryButton>
+        <SecondaryButton modifiers='large'>Goodbye world</SecondaryButton>
+        <TertiaryButton modifiers={["success"]}>Hey world</TertiaryButton>
+      </div>
+      <SignUpModal />
+      <GlobalStyle />
+    </ThemeProvider>
+
+  )
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
